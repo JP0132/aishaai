46ea8b55fa0e3f214024f35044482fdc3fb2c69b
JP0132 <jaynikparsotomo02@gmail.com>
2024-06-03 17:17:57 +0100

    Changing to make all routes public for now

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/api/webhooks/clerk"]);
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware((auth, request) => {});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
